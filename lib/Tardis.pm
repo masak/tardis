e28e53ed39f42741b792bce074ac23e76973e4a6
Carl Masak <cmasak@gmail.com>
2010-03-07 11:47:27 +0100

    [Tardis::Debugger] now collects vars into pad
    At 'compile time', the AST is now traversed and all the declared variables
    collected. No error detection is done for either (1) variables being used but
    not declared, or (2) variables being declared after they're used. But this
    works for now.

--- lib/Tardis.pm
+++ lib/Tardis.pm
@@ -1,15 +1,74 @@
 use v6;
 
-class AST::Node::Declaration {}
-class AST::Node::Assignment {}
-class AST::Node::Variable {}
-class AST::Node::Literal {}
-class AST::Node::Op {}
+class AST::Node {
+    method traverse(&callback) {
+        die "No .traverse method specified for {self.WHAT}";
+    }
+}
+
+class AST::Node::Statementlist is AST::Node {
+    has AST::Node @.statements;
+
+    method new(AST::Node *@statements) {
+        self.bless(self.CREATE, :@statements);
+    }
+
+    method traverse(&callback) {
+        .traverse(&callback) for @!statements;
+        &callback(self);
+    }
+}
+
+class AST::Node::Variable is AST::Node {
+    has Str $.name;
+
+    method new(Str $name) {
+        self.bless(self.CREATE, :$name);
+    }
+
+    method traverse(&callback) {
+        &callback(self);
+    }
+}
+
+class AST::Node::Declaration is AST::Node {
+    has AST::Node::Variable $.variable;
+
+    method new(Str $type, AST::Node::Variable $variable) {
+        self.bless(self.CREATE, :$variable);
+    }
+
+    method traverse(&callback) {
+        $!variable.traverse(&callback);
+        &callback(self);
+    }
+}
+
+class AST::Node::Assignment is AST::Node {
+    has AST::Node $.lhs;
+    has AST::Node $.rhs;
+
+    method new(AST::Node $lhs, AST::Node $rhs) {
+        self.bless(self.CREATE, :$lhs, :$rhs);
+    }
+
+    method traverse(&callback) {
+        $!lhs.traverse(&callback);
+        $!rhs.traverse(&callback);
+        &callback(self);
+    }
+}
+
+class AST::Node::Literal is AST::Node {
+    method traverse(&callback) {
+        &callback(self);
+    }
+}
+
+class AST::Node::Op is AST::Node {}
 
 class Tardis::Pad {
-    method variables() {
-        0
-    }
+    has Str @.variables;
 
     method variable($name) {
         'Any'
@@ -17,19 +76,26 @@ class Tardis::Pad {
 }
 
 class Tardis::Tick {
-    method pad() {
-        Tardis::Pad.new();
-    }
+    has Tardis::Pad $.pad;
 }
 
 class Tardis::Debugger {
-    has @!program;
+    has AST::Node::Statementlist $!program;
     has Tardis::Tick @.ticks;
+    has Tardis::Pad $!pad; # XXX: but there is more than one pad.
+
+    submethod BUILD($!program) {
+        my @variables;
+        $!program.traverse: method {
+            if self ~~ AST::Node::Declaration {
+                push @variables, $.variable.name;
+            }
+        };
+        $!pad = Tardis::Pad.new(:@variables);
+    }
 
     method run() {
-        @!ticks.push( Tardis::Tick.new() );     # empty starting state
-        for @!program -> $statement {
-            @!ticks.push( Tardis::Tick.new() ); # state after statement
-        }
+        @!ticks.push( Tardis::Tick.new(:pad($!pad)) );
+        @!ticks.push( Tardis::Tick.new(:pad($!pad)) ) for $!program.statements;
     }
 }
--- t/examples.t
+++ t/examples.t
@@ -3,20 +3,24 @@ use Test;
 
 use Tardis;
 
-my Tardis::Debugger $debugger .= new(program => ());
+my Tardis::Debugger $debugger .= new(program => AST::Node::Statementlist.new());
 $debugger.run;
 
 is +$debugger.ticks, 1, 'empty program has 1 tick';
 is +$debugger.ticks[0].pad.variables, 0,
    'there are no variables in the current pad';
 
-$debugger .= new(program => [AST::Node::Assignment.new(
-    AST::Node::Declaration.new(
-        'my',
-        AST::Node::Variable.new('$a')
-    ),
-    AST::Node::Literal.new(42)
-)]);
+$debugger .= new(
+    program => AST::Node::Statementlist.new(
+        AST::Node::Assignment.new(
+            AST::Node::Declaration.new(
+                'my',
+                AST::Node::Variable.new('$a')
+            ),
+            AST::Node::Literal.new(42)
+        )
+    )
+);
 $debugger.run;
 
 is +$debugger.ticks, 2, 'single-assignment program has 2 ticks';
@@ -31,15 +35,20 @@ is +$debugger.ticks, 2, 'single-assignment program has 2 ticks';
        'after assignment, var is 42';
 }
 
-$debugger .= new(program => [AST::Node::Assignment.new(
-    AST::Node::Declaration.new(
-        'my',
-        AST::Node::Variable.new('$a')
-    ),
-    AST::Node::Op.new(
-        'postfix:<++>',
-        AST::Node::Variable.new('$a')))
-]);
+$debugger .= new(
+    program => AST::Node::Statementlist.new(
+        AST::Node::Assignment.new(
+            AST::Node::Declaration.new(
+                'my',
+                AST::Node::Variable.new('$a')
+            ),
+            AST::Node::Op.new(
+                'postfix:<++>',
+                AST::Node::Variable.new('$a')
+            )
+        )
+    )
+);
 $debugger.run;
 
 is +$debugger.ticks, 3, 'assignment with ++ has 3 ticks';
